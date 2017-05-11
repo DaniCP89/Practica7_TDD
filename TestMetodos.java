6a3aa5285d8ec83b2b7e2c9c5a37ef5d263a16ba
DaniCP89 <daniel.cp.89@gmail.com>
2017-05-11 13:01:30 +0200

    Primer Commit

---
+++ TestMetodos.java
@@ -0,0 +1,14 @@
+package prPractica7_TDD;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestMetodos {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
